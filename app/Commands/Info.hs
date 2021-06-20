cfc2cc4a8d0b156dbd6f52888a91ba0b57d77015
ggzor <30713864+ggzor@users.noreply.github.com>
2021-06-20 14:31:32 -0500

    Adding better error reporting

--- app/Commands/Info.hs
+++ app/Commands/Info.hs
@@ -1,43 +1,38 @@
 module Commands.Info where
 
 import Control.Monad (forM_, void)
-import Data.Text (Text)
 import Fmt (fmt, fmtLn, (+|), (|+))
 import Math.Primes (primes)
 import Parser
-import Text.Megaparsec (parseMaybe)
+import TuringMachines.Core
 import TuringMachines.Graphviz
 import TuringMachines.Normalize (getProgram, normalize)
 import TuringMachines.Numbering
-import qualified TuringMachines.Parser as TP
 import Utils
 
-processInfo :: InfoOptions -> Text -> IO ()
-processInfo (InfoOptions format) t =
-  case parseMaybe TP.pProgram t of
-    Nothing -> putStrLn "El programa no es valido"
-    Just p -> do
-      let label = if length format > 1 then putStrLn else void . pure
-      let normalized = normalize p
+processInfo :: InfoOptions -> Program Integer -> IO ()
+processInfo (InfoOptions format) p = do
+  let label = if length format > 1 then putStrLn else void . pure
+  let normalized = normalize p
 
-      if format == [Normalized, Graph]
-        then fmtLn $ generateGraph (getProgram normalized) |+ ""
-        else do
-          forM_ format $ \case
-            Original -> do
-              label "Original: "
-              putStrLn $ programAsStr p
-            Normalized -> do
-              label "Normalizado: "
-              putStrLn $ programAsStr (getProgram normalized)
-            Number -> do
-              label "Numero de Godel:"
-              print $ programAsNumber normalized
-            PrimeSeq -> do
-              label "Secuencia de potencias de primos: "
-              putStrLn $
-                unwords . zipWith (\prime n -> fmt $ prime |+ "^" +| n |+ "") primes $
-                  programAsSequence normalized
-            Graph -> do
-              label "Grafo: "
-              fmtLn $ generateGraph p |+ ""
+  if format == [Normalized, Graph]
+    then fmtLn $ generateGraph (getProgram normalized) |+ ""
+    else do
+      forM_ format $ \case
+        Original -> do
+          label "Original: "
+          putStrLn $ programAsStr p
+        Normalized -> do
+          label "Normalizado: "
+          putStrLn $ programAsStr (getProgram normalized)
+        Number -> do
+          label "Numero de Godel:"
+          print $ programAsNumber normalized
+        PrimeSeq -> do
+          label "Secuencia de potencias de primos: "
+          putStrLn $
+            unwords . zipWith (\prime n -> fmt $ prime |+ "^" +| n |+ "") primes $
+              programAsSequence normalized
+        Graph -> do
+          label "Grafo: "
+          fmtLn $ generateGraph p |+ ""
--- app/Main.hs
+++ app/Main.hs
@@ -8,8 +8,10 @@ import Fmt ((+|), (|+))
 import Options.Applicative hiding (action)
 import Parser
 import RIO (readFileUtf8)
+import System.Console.ANSI
 import System.Directory (doesFileExist)
-import Text.Megaparsec (parseMaybe)
+import Text.Megaparsec (errorBundlePretty, parse)
+import TuringMachines.Core
 import qualified TuringMachines.Parser as TP
 import Utils
 
@@ -25,11 +27,13 @@ main = doWork =<< execParser opts
 
 doWork :: Commands -> IO ()
 doWork (Numbered n) = processNumbered n
-doWork (Info path opts) = withExistentFile path (processInfo opts)
-doWork (Eval path input opts) = withExistentFile path $ \pathText -> do
-  case parseMaybe TP.pProgram pathText of
-    Just program -> processEval opts program input
-    Nothing -> exitError "El programa no es valido"
+doWork (Info path opts) =
+  withExistentFile path \pathText ->
+    withValidProgram path pathText (processInfo opts)
+doWork (Eval path input opts) =
+  withExistentFile path \pathText ->
+    withValidProgram path pathText \program ->
+      processEval opts program input
 
 withExistentFile :: FilePath -> (Text -> IO ()) -> IO ()
 withExistentFile path action = do
@@ -37,3 +41,14 @@ withExistentFile path action = do
   if exists
     then readFileUtf8 path >>= action
     else exitError $ "El archivo '" +| path |+ "' no existe"
+
+withValidProgram :: FilePath -> Text -> (Program Integer -> IO ()) -> IO ()
+withValidProgram path programText action = do
+  case parse TP.pProgram path programText of
+    Right program -> action program
+    Left err -> do
+      setSGR [SetColor Foreground Vivid Red, SetConsoleIntensity BoldIntensity]
+      putStr "error: "
+      setSGR [Reset]
+      putStrLn "El programa no es válido"
+      putStr (errorBundlePretty err)

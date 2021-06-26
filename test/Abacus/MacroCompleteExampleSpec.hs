4db9000725a58f4568ca4ab58c2b042ddd6beac6
ggzor <30713864+ggzor@users.noreply.github.com>
2021-06-26 08:23:42 -0500

    Adding complete macro example bound to file

--- test/Abacus/MacroCompleteExampleSpec.hs
+++ test/Abacus/MacroCompleteExampleSpec.hs
@@ -16,12 +16,17 @@ finalExampleInputs =
   , ("copy", (["t"], 0))
   , ("sum", ([], 1))
   , ("mult", (["i", "acc"], 1))
+  , ("exp", (["i", "acc"], 3))
   , ("monus", (["t"], 0))
   , ("abs_minus", (["t1", "t2"], 1))
   , ("sg", (["t"], 0))
   , ("sgn", (["s", "t"], 1))
   , ("rm", (["c", "t1", "t2", "t3"], 3))
   , ("div", (["t"], 7))
+  , ("ndiv", (["c", "z"], 8))
+  , ("equal", (["t"], 3))
+  , ("prime", (["t", "two"], 10))
+  , ("nthprime", (["c", "t"], 12))
   ]
 
 spec :: Spec
--- test/MacroUtils.hs
+++ test/MacroUtils.hs
@@ -4,95 +4,13 @@ import Abacus.Macro
 import Abacus.Parser
 import Control.Monad.Except
 import Data.Text (Text)
+import qualified Data.Text.IO as TIO
+import GHC.IO.Unsafe (unsafePerformIO)
 import Text.Megaparsec (parseMaybe)
-import Text.RawString.QQ
 
 completeProgramSource :: Text
-completeProgramSource =
-  [r|
-zero(x):
-  s:
-    x- e
-    s
-  e:
-
-trans(x, y):
-  s:
-    x- e
-    y+
-    s
-  e:
-
-dup(x, y, z):
-  s:
-    x- e
-    y+
-    z+
-    s
-  e:
-
-copy(x, y):
-  dup(x, y, t)
-  trans(t, x)
-
-sum(x, y, r):
-  copy(x, r)
-  copy(y, r)
-
-mult(x, y, r):
-  copy(x, i)
-  s:
-    i- e
-    sum(y, r, acc)
-    trans(acc, r)
-    s
-  e:
-
-monus(x, y, r):
-  trans(x, r)
-  trans(y, t)
-  s:
-    t- e
-    r- e
-    s
-  e:
-    zero(t)
-
-abs_minus(x, y, r):
-  monus(x, y, t1)
-  monus(y, x, t2)
-  sum(t1, t2, r)
-
-sg(x, r):
-  trans(x, t)
-  t- e
-  r+
-  e:
-    zero(t)
-
-sgn(x, r):
-  sg(x, s)
-  t+
-  monus(t, s, r)
-  zero(t)
-  zero(s)
-
-rm(x, y, r):
-  copy(y, c)
-  l:
-    c- e
-    r+
-    abs_minus(x, r, t1)
-    sg(t1, t2)
-    mult(r, t2, t3)
-    trans(t3, r)
-
-div(x, y, r):
-  rm(x, y, t)
-  sgn(t, r)
-  zero(t)
-
-|]
+{-# NOINLINE completeProgramSource #-}
+completeProgramSource = unsafePerformIO $ TIO.readFile "examples/abacus.txt"
 
 completeProgramIndex :: MacroIndex
 completeProgramIndex = createIndexOf completeProgramSource
